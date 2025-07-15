8afdf78bc172e224291ca73313042bebca37f5a8
mrs-liliia-kutova <lilqatester@gmail.com>
2025-07-15 14:33:33 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,40 @@ public class Main {
 
         // Тут має розгортатися епічна битва між Супергероями
 
+        SuperHero flash = new SuperHero();
+        SuperHero ironMan = new SuperHero();
+        SuperHero jackieChan = new SuperHero();
+
+        flash.name ="flash";
+        flash.attackPower=20;
+        flash.deffencePower=10;
+        flash.health=100;
+        flash.isAlive=true;
+        flash.heroElement = "water";
+
+        ironMan.name="ironMan";
+        ironMan.attackPower=15;
+        ironMan.deffencePower=5;
+        ironMan.health=100;
+        ironMan.isAlive=true;
+        ironMan.heroElement = "fire";
+
+        jackieChan.name="Jackie";
+        jackieChan.attackPower=15;
+        jackieChan.deffencePower=10;
+        jackieChan.health=100;
+        jackieChan.isAlive=true;
+        jackieChan.heroElement = "air";
+
+        System.out.println("Let's the game begin");
+
+        flash.attack(ironMan, 25);
+        ironMan.showStats();
+
+
+
+//        while (flash.isAlive && ironMan.isAlive) {
+//            int attackStrength = //
+        }
     }
-}
+
