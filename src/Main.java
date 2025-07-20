9f5b0e5479e3ba47fca22a19a77f5afa9b52468e
mrs-liliia-kutova <lilqatester@gmail.com>
2025-07-20 23:09:22 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,41 +7,41 @@ public class Main {
         SuperHero ironMan = new SuperHero();
         SuperHero jackieChan = new SuperHero();
 
-        flash.name ="flash";
-        flash.attackPower=20;
-        flash.deffencePower=10;
-        flash.health=100;
+        flash.name = "flash";
+        flash.attackPower = 20;
+        flash.deffencePower = 10;
+        flash.health = 100;
 
-        ironMan.name="ironMan";
-        ironMan.attackPower=15;
-        ironMan.deffencePower=5;
-        ironMan.health=100;
+        ironMan.name = "ironMan";
+        ironMan.attackPower = 15;
+        ironMan.deffencePower = 5;
+        ironMan.health = 100;
 
-        jackieChan.name="Jackie";
-        jackieChan.attackPower=15;
-        jackieChan.deffencePower=10;
-        jackieChan.health=100;
+        jackieChan.name = "Jackie";
+        jackieChan.attackPower = 15;
+        jackieChan.deffencePower = 10;
+        jackieChan.health = 100;
 
         System.out.println("Let's the game begin");
 
-        while (flash.isAlive() && ironMan.isAlive()){
-        flash.attack(ironMan, 25);
-        if (!ironMan.isAlive()) break;
-        ironMan.attack(flash, 10);
-        if (!flash.isAlive()) break;
-        }
-        
-        System.out.println("Let's the game begin");
-        while (flash.isAlive() && jackieChan.isAlive()){
-            flash.attack(jackieChan, 15);
-            if (!jackieChan.isAlive()) break;
-            jackieChan.attack(flash, 25);
-            if (!flash.isAlive()) break;
+        while (flash.isAlive() && ironMan.isAlive()) {
+            flash.attack(ironMan, 25);
+            if (ironMan.isAlive()) {
+                ironMan.attack(flash, 10);
+            }
         }
 
+            if (flash.isAlive()) {
+                System.out.println("Let's the second game begin");
+                while (flash.isAlive() && jackieChan.isAlive()) {
+                    flash.attack(jackieChan, 15);
+                    if (jackieChan.isAlive()) ;
+                    jackieChan.attack(flash, 25);
+                }
 
 
-
+            }
+        }
     }
-    }
+
 
